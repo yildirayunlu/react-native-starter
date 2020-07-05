206d283b4173fe15d4dec4a2a07f0658642c05aa
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2020-07-05 18:31:35 +0300

    add material icons

--- src/screens/Feed/index.tsx
+++ src/screens/Feed/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Box, Text } from '../../components';
 
@@ -7,7 +7,7 @@ export const Feed: React.FC = () => {
   return (
     <Box>
       <Text>Feed Screen</Text>
-      <Icon name="rocket" size={30} />
+      <Icon name="check-circle" size={30} />
     </Box>
   );
 };
