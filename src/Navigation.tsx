bf3317303781c0d124b84132e6c069228a53b343
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2020-07-05 15:38:13 +0300

    add feedstack and profilestack navigator

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,16 +1,31 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import { Feed, Profile } from './screens';
 
 const Navigation = () => {
   const TabNavigator = createBottomTabNavigator();
+  const StackNavigator = createStackNavigator();
+
+  const FeedStack = () => (
+    <StackNavigator.Navigator>
+      <StackNavigator.Screen name="Feed" component={Feed} />
+    </StackNavigator.Navigator>
+  );
+
+  const ProfileStack = () => (
+    <StackNavigator.Navigator>
+      <StackNavigator.Screen name="Profile" component={Profile} />
+    </StackNavigator.Navigator>
+  );
+
   return (
     <NavigationContainer>
       <TabNavigator.Navigator>
-        <TabNavigator.Screen name="Feed" component={Feed} />
-        <TabNavigator.Screen name="Profile" component={Profile} />
+        <TabNavigator.Screen name="Feed" component={FeedStack} />
+        <TabNavigator.Screen name="Profile" component={ProfileStack} />
       </TabNavigator.Navigator>
     </NavigationContainer>
   );
