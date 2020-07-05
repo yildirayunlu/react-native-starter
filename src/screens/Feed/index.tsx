df8f308e6dc9f2e8a9509144e51477a0411db24a
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2020-07-05 18:17:46 +0300

    add react-native-vector-icons lib

--- src/screens/Feed/index.tsx
+++ src/screens/Feed/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import { Box, Text } from '../../components';
 
@@ -6,6 +7,7 @@ export const Feed: React.FC = () => {
   return (
     <Box>
       <Text>Feed Screen</Text>
+      <Icon name="rocket" size={30} />
     </Box>
   );
 };
